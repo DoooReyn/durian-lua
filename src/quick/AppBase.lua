81a9b89458fcb17eb741e7650ea7400be628ad62
DoooReyn <jl88744653@gmail.com>
2020-10-30 16:50:27 +0800

    记录前后台切换时间差

--- src/quick/AppBase.lua
+++ src/quick/AppBase.lua
@@ -1,18 +1,29 @@
 local AppBase = GG.Magic.Class("AppBase")
 
 function AppBase:ctor()
+    -- 进入后台的时间
+    self.__time = nil
+
+    -- 监听进入后台事件
     local customListenerBg = cc.EventListenerCustom:create("APP_ENTER_BACKGROUND_EVENT", function()
+        print("App Enter Background: ", self.__time)
+        self.__time = os.time()
         GG.Audio.pauseAll()
         self:onEnterBackground()
     end)
+
+    -- 监听进入前台事件
     GG.S_EventDipatcher:addEventListenerWithFixedPriority(customListenerBg, 1)
     local customListenerFg = cc.EventListenerCustom:create("APP_ENTER_FOREGROUND_EVENT", function()
         GG.Audio.resumeAll()
+        local leave_time = os.time() - self.__time
+        self.__time = nil
+        print("App Enter Foreground: ", leave_time)
         self:onEnterForeground()
     end)
     GG.S_EventDipatcher:addEventListenerWithFixedPriority(customListenerFg, 1)
 
-    -- set global app
+    -- 将App设置为全局可访问
     GG.S_App = self
 end
 
@@ -37,6 +48,10 @@ function AppBase:createView(viewName, ...)
     local viewPackageName = "app.views." .. viewName
     local viewClass = GG.Requires(viewPackageName)
     return viewClass.new(...)
+end
+
+function AppBase:refresh()
+    
 end
 
 -- override me
